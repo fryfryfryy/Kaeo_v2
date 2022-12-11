b6a561137ef087c851e62efe3cf021d0bf313185
Harry <harryfry11@gmail.com>
2022-12-11 20:35:19 +0000

    still messing around with menus and multipages

--- Home.py
+++ Home.py
@@ -2,6 +2,8 @@ from textwrap import fill
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+
+
 #Logo
 st.sidebar.image("keepaneyeon-logos_black.png", use_column_width=True)
 
@@ -15,10 +17,10 @@ hide_menu_style = """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 #streamlit option menu component (https://github.com/victoryhb/streamlit-option-menu) Icons are bootstrapped. 
-with st.sidebar:
-                selected = option_menu(
-                    menu_title = "Keep an Eye on", 
-                    options = ["Location Analysis", "Police Feed", "Sentiment Analysis"],
-                    menu_icon = "eye-fill",
-                    icons=["geo-alt-fill", "rss-fill", "twitter"]
-                    )
+#with st.sidebar:
+                #selected = option_menu(
+                    #menu_title = "Keep an Eye on", 
+                    #options = ["Location Analysis", "Police Feed", "Sentiment Analysis"],
+                    #menu_icon = "eye-fill",
+                    #icons=["geo-alt-fill", "rss-fill", "twitter"]
+                    #)
---
+++ pages/1_Police_Feed.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import time
+import numpy as np
+
+st.sidebar.image("keepaneyeon-logos_black.png", use_column_width=True)
