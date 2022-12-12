5cdffcd1658aa6cfc8ca6316c6eea0036de32f4f
Harry <harryfry11@gmail.com>
2022-12-12 19:48:02 +0000

    Changes to crime analysis code

--- pages/Crime_Analysis.py
+++ pages/Crime_Analysis.py
@@ -1,12 +1,19 @@
 import streamlit as st
 import requests
+import pandas as pd
+import pydeck as pdk
+import plotly.express as px
 
 st.sidebar.image("keepaneyeon-logos_black.png", use_column_width=True)
 
 @st.cache
 def grab_postcode(postcode):
     """
-    Function that takes a postcode and build a polygon based on the coordinates of the area around it.
+    This code defines a function called grab_postcode() that takes in a postcode as its argument. 
+    The function uses the requests module to make an API request to the "http://api.postcodes.io/postcodes/" endpoint using the provided postcode.
+    If the API response is successful (indicated by a status code of 200), the function extracts the latitude and longitude coordinates from the response and uses them to make additional API requests to the "https://data.police.uk/api/locate-neighbourhood" and "https://data.police.uk/api/{force}/{neigh}/boundary" endpoints. The results of these requests are then used to build a polygon that represents the area around the provided postcode.
+    If the initial API request is not successful, the function raises an exception. In either case, the function returns a tuple containing the latitude and longitude coordinates and the polygon representing the area around the postcode.
+    The code uses the pandas module to convert the API response data to a DataFrame object, which is then used to extract the coordinates and build the polygon. 
     """
     response = requests.get(f'http://api.postcodes.io/postcodes/{postcode}')
     if response.ok:
@@ -22,3 +29,32 @@ def grab_postcode(postcode):
     else:
         raise Exception
     return df, polygon
+
+
+def grab_dates(poly, date):
+    """
+    Function that fetches the date range of the available data. St Cache avoides any unnecessary api calls. 
+    """
+    response = requests.get('https://data.police.uk/api/crimes-street-dates')
+    if response.ok:
+        df = pd.DataFrame.from_dict(response.json())
+        df = df.explode('stop-and-search')
+    else:
+        df = "Data Refresh failed, please refresh the page"
+    return df
+
+@st.cache
+def grab_force(coordinates):
+    """
+    Function to match the postcodes coordinates to the police force of the area and the relevant people in it.
+    """
+    ids = requests.get(f'https://data.police.uk/api/locate-neighbourhood?q={coordinates[0]},{coordinates[1]}').json()['force']
+    response = requests.get(f'https://data.police.uk/api/forces/{ids}')
+    response2 = requests.get(f'https://data.police.uk/api/forces/{ids}/people')
+    if response.ok and response2.ok:
+        df = response.json()
+        df2 = response2.json()
+    else:
+        df = f"Data pull failed with error{response.text}, please refresh the page"
+        df2=''
+    return df, df2
