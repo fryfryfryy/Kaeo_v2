1d18fe664abafc898f0bde62c134602b91ca14e1
Harry <harryfry11@gmail.com>
2022-12-19 16:25:03 +0000

    more work on main crime profile page

--- pages/Crime_Analysis.py
+++ pages/Crime_Analysis.py
@@ -10,7 +10,9 @@ postcode = st.sidebar.text_input(
                 'Please Input Full Postcode',
                 'NN4 9YX'
             )
-coordinates = [51.509865, -0.118092]
+code = [51.509865, -0.118092]
+poly = '51.5201931,-0.1284721:51.5145315,-0.1272705:51.5149054,-0.1130226:51.5206203,-0.1149109:51.5201931:-0.1284721'
+
 @st.cache(ttl=86400)
 def grab_dates():
     """
@@ -70,3 +72,37 @@ def grab_postcode(postcode):
     else:
         raise Exception
     return df, polygon
+
+def grab_data(coordinates=None, date=None):
+                """
+                Function to fetch the list of individual crimes that happened in the area 
+                """
+                response = requests.get(f"https://data.police.uk/api/crimes-street/all-crime?date={date}&poly={coordinates}")
+                if response.ok and len(response.json()) > 0:   
+                    df = pd.DataFrame.from_dict(response.json())
+                    df['latitude'] = df['location'].apply(lambda x: float(x.get('latitude')))
+                    df['longitude'] = df['location'].apply(lambda x: float(x.get('longitude')))
+                    df['last_outcome'] = df['outcome_status'].apply(lambda x: x.get('category') if type(x) is dict else None)
+                else:
+                    df = []
+                return df
+
+@st.cache(allow_output_mutation=True)
+def period_data(dates, range, poly):
+    """
+    Function that fetches crimes over a set number of periods
+    """
+    df_t = pd.DataFrame()
+    for date in dates[:range]:
+        df = grab_data(poly,date)
+        df_t = df_t.append(df)
+    return df_t
+
+try:
+    code,poly = grab_postcode(postcode)
+except:
+    st.error('Postcode Could Not be Matched')
+    
+force_prof, force_people = grab_force(code)
+
+st.write(force_prof, force_people)
